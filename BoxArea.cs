47726daa7609f2b4fd7e1974eb238494bd52ef51
BarSimanTov <barst100@gmail.com>
2025-08-02 13:47:32 +0300

    add box

---
+++ BoxArea.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+
+public partial class BoxArea : InterctableAria
+{
+
+	private AnimatedSprite2D _animatedSprite;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		//_animatedSprite.Play("Idle");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override  void interactWith()
+	{
+	
+		DerstroyBox();
+		
+	}
+	
+	private async void DerstroyBox(){
+		_animatedSprite.Play("interact");
+
+		 await ToSignal(GetTree().CreateTimer(0.75), "timeout"); // non-blocking wait
+
+		GetParent().QueueFree();
+	}
+
+}
