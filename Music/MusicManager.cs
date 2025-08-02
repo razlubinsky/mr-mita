6aabf427135e7dce5564bcc8df3abeba6906946e
Raz <razlubinsky@gmail.com>
2025-08-02 17:07:44 +0300

    fixed music manager

--- Music/MusicManager.cs
+++ Music/MusicManager.cs
@@ -3,20 +3,8 @@ using System;
 
 public partial class MusicManager : Node2D
 {
-	private static MusicManager _instance;
-
 	public override void _Ready()
 	{
-		if (_instance != null && _instance != this)
-		{
-			QueueFree(); // Avoid duplicates
-		}
-		else
-		{
-			_instance = this;
-			GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D").Play();
-			GetTree().Root.AddChild(this); // Move to root to persist
-			this.Owner = null;
-		}
+		GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D").Play();
 	}
 }
